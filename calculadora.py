b3e7f885205ebc8a7c341e7e44a0774659ccf270
cristinarv <35956460+cristinarv@users.noreply.github.com>
2018-02-05 20:18:50 +0100

    Intento cambiarlo desde aqui

--- calculadora.py
+++ calculadora.py
@@ -1,28 +1,26 @@
-"""Cristina del Río Vergel. 
-Ejercico 2 de python: Hacer una calculadora #!/usr/bin/python3"""
-
+"""Cristina del Río Vergel.
+Ejercico 2 de python: Hacer una calculadora!/usr/bin/"""
 import sys
-#extracción de argumentos
-try:   
-    funcion= sys.argv[1]    
-    operando1= float(sys.argv[2])
-    operando2= float(sys.argv[3])
+# extracción de argumentos
+try:
+    funcion = sys.argv[1]
+    operando1 = float(sys.argv[2])
+    operando2 = float(sys.argv[3])
 except ValueError:
     sys.exit("Error, sólo puedes meter operandos que sean números.")
-    
-#control de argumentos
-if len(sys.argv)!= 4:    
-	sys.exit("Argumentos inválidos. Argumentos correctos:(python3 calculadora.py funcion operando1 operando2)")
-elif funcion== "sumar":   
+# control de argumentos
+if len(sys.argv) != 4:
+    sys.exit("Argumentos inválidos:(python3 calculadora.py funcion operando1 operando2)")
+elif funcion == "sumar":
     print("El resultado de la suma es: ", operando1 + operando2)
-elif funcion== "restar":
+elif funcion == "restar":
     print("El resultado de la resta es: ", operando1 - operando2)
-elif funcion== "multiplicar":
+elif funcion == "multiplicar":
     print("El resultado de la multiplicación es: ", operando1 * operando2)
-elif funcion== "division":
+elif funcion == "division":
     try:
         print("El resultado de la división es: ", operando1 / operando2)
     except ZeroDivisionError:
-        print ("Error, estás dividiendo entre cero")
+        print("Error, estás dividiendo entre cero")
 else:
-    print("NO se acepta este tipo de función, pruebe con(sumar, restar, multiplicar, dividir)") 
+    print("NO se acepta este tipo de función, pruebe con(sumar, restar, multiplicar, dividir)")
